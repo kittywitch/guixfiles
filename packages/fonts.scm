3e0e47b3d5066cf431d205d39fca4e4c72f49f95
Kat Inskip <kat@inskip.me>
2026-02-05 10:21:22 -0800

    fix: remove unnecessary hashes

--- packages/fonts.scm
+++ packages/fonts.scm
@@ -11,9 +11,7 @@
        (uri (git-reference
              (url "https://github.com/googlefonts/atkinson-hyperlegible-mono")
              (commit "154d50362016cc3e873eb21d242cd0772384c8f9")))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0000000000000000000000000000000000000000000000000000"))))
+       (file-name (git-file-name name version))))
     (build-system font-build-system)
     (home-page "https://www.brailleinstitute.org/freefont/")
     (synopsis "New (2024) monospace sibling family to Atkinson Hyperlegible Next")
@@ -31,9 +29,7 @@
        (uri (git-reference
              (url "https://github.com/googlefonts/atkinson-hyperlegible-next")
              (commit "7925f50f649b3813257faf2f4c0b381011f434f1")))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0000000000000000000000000000000000000000000000000000"))))
+       (file-name (git-file-name name version))))
     (build-system font-build-system)
     (home-page "https://www.brailleinstitute.org/freefont/")
     (synopsis "New (2024) second version of the Atkinson Hyperlegible fonts")
